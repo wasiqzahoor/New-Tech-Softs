1f4ab28c61e64b60cef98a665a280e2efcb8ab8c
wasiqzahoor <wasiqzohoor1234@gmail.com>
2026-04-23 16:59:49 +0500

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import Sitemap from 'vite-plugin-sitemap'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://newtechsofts.com', // Apni website ka link
+      dynamicRoutes: ['/about', '/services', '/blog','portfolio','/blog/1','/blog/2','/blog/3','/blog/4','/blog/5'], // Agar dynamic pages hain
+    })
+  ]
 })
