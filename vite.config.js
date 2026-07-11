7285533229c03af388b5c9e4776de5b26d0c8abe
wasiqzahoor <wasiqzahoor1234@gmail.com>
2026-07-11 10:50:57 +0500

    fix deployment error2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-   plugins: [
-    react(),
-    Sitemap({
-      hostname: 'https://newtechsofts.com', // Apni website ka link
-      dynamicRoutes: ['/about', '/services', '/blog','/portfolio','/blog/1','/blog/2','/blog/3','/blog/4','/blog/5'], // Agar dynamic pages hain
-    })
-  ]
-})
+  plugins: [react()],
+});
